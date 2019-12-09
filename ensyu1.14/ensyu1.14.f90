cb03619f0e2837df150809f6f997d0aa5dcb201b
kjakx <fxhfr163@ybb.ne.jp>
2019-12-09 01:56:01 +0000

    ensyu1.14

--- ensyu1.14/ensyu1.14.f90
+++ ensyu1.14/ensyu1.14.f90
@@ -1,11 +1,11 @@
 program e_taylor
-    real(8) :: e_approx = 1, e = exp(1.0d0)
+    real(8) :: e_approx = 1, e = exp(1.0d0), er
     integer :: n, fact_n = 1
     do n = 1, 10
         fact_n = fact_n * n
-        e_approx = e_approx / fact_n
-        er = abs(e_approx - er)
+        e_approx = e_approx + (1.0d0 / fact_n)
+        er = abs(e_approx - e)
         write(*, *) 'e_approx(', n, ') =', e_approx
-        write(*, *) 'er(', n, ') =', er 
+        write(*, *) 'er(', n, ') =', er
     enddo
 end program e_taylor
