fcef0c18a4379b38b4610087d364b14f5ea7ea4f
kjakx <fxhfr163@ybb.ne.jp>
2019-12-13 07:51:31 +0000

    19/12/13

--- ensyu1.16/ensyu1.16.f90
+++ ensyu1.16/ensyu1.16.f90
@@ -1,7 +1,7 @@
 program disp_rel
-    real(8) :: L, T, h, g = 9.80665, pi = 3.14159265
+    real(8) :: L, L2, T, h, g = 9.80665, pi = 3.14159265
     integer :: n, n_max = 100
-    write(*, '(\a)') 'input T, h :'
+    write(*, *) 'input T[s], h[m] :'
     read(*, *) T, h
     if (T <= 0) stop 'stop. T must be T > 0'
     if (h <= 0) stop 'stop. h must be h > 0'
@@ -10,5 +10,5 @@ program disp_rel
         L2 = T ** 2 * (g*L/(2*pi)*tanh(2*pi*h/L))
         L = sqrt(L2)
     enddo 
-    write(*, *) 'L =', L
+    write(*, *) 'L =', L,'[m]'
 end program disp_rel
