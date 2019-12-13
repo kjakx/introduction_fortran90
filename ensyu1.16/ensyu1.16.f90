59ac8067cd75181b43f8ad3f81ffc114291f5bb2
kjakx <43692824+kjakx@users.noreply.github.com>
2019-12-13 15:06:06 +0900

    Update ensyu1.16.f90

--- ensyu1.16/ensyu1.16.f90
+++ ensyu1.16/ensyu1.16.f90
@@ -1,8 +1,8 @@
 program disp_rel
     real(8) :: L, T, h, g = 9.80665, pi = 3.14159265
     integer :: n, n_max = 100
-    write(*, '(\a)') 'input L, h :'
-    read(*, *) L, h
+    write(*, '(\a)') 'input T, h :'
+    read(*, *) T, h
     if (L <= 0) stop 'stop. L must be L > 0'
     if (h <= 0) stop 'stop. h must be h > 0'
     L = h
