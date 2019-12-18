bb5b84ae8e5f6e1154fb2dc092abaf83c60f398b
kjakx <fxhfr163@ybb.ne.jp>
2019-12-18 05:14:59 +0000

    saved@Wed 18 Dec 2019 05:14:58 AM UTC

--- ensyu1.22/ensyu1.22.f90
+++ ensyu1.22/ensyu1.22.f90
@@ -1,23 +1,29 @@
 program quad_eq
     implicit none
-    double precision a, b, c, D
-    double complex x1, x2
+    double precision a, b, c, D, x1_re, x1_im, x2_re, x2_im
     write(*, *) 'input a, b, c:'
     read(*, *) a, b, c
     D = b * b - 4.0d0 * a * c
-    if (abs(sqrt(D) - b) > abs(- sqrt(D) - b)) then
-        x1 = (- b + sqrt(D)) / (2.0d0 * a)
-        x2 = - (c / a) / x1
-    else
-        x2 = (- b - sqrt(D)) / (2.0d0 * a)
-        x1 = - (c / a) / x2
+    if (D >= 0) then
+        if (abs(sqrt(D) - b) > abs(- sqrt(D) - b)) then
+            x1_re = (- b + sqrt(D)) / (2.0d0 * a)
+            x2_re = - (c / a) / x1_re
+        else
+            x2_re = (- b - sqrt(D)) / (2.0d0 * a)
+            x1_re = - (c / a) / x2_re
+        endif
+    else ! (D < 0)
+        x1_re = -b / (2.0d0 * a)
+        x1_im = sqrt(-D) / (2.0d0 * a)
+        x2_re = x1_re
+        x2_im = -x1_im
     endif
     write(*, *) 'D =', D
     if (D < 0) then
-        write(*, *) 'x1 =', x1
-        write(*, *) 'x2 =', x2
+        write(*, *) 'x1 =', cmplx(x1_re, x1_im, 8)
+        write(*, *) 'x2 =', cmplx(x2_re, x2_im, 8)
     else
-        write(*, *) 'x1 =', real(x1)
-        write(*, *) 'x2 =', real(x2)    
+        write(*, *) 'x1 =', real(x1_re)
+        write(*, *) 'x2 =', real(x2_re)    
     endif
 end program quad_eq
