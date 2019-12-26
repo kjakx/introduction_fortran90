93b57364c09ab75ca0698b6990962ac1bfbadb7a
kjakx <fxhfr163@ybb.ne.jp>
2019-12-26 06:50:18 +0000

    saved@Thu 26 Dec 2019 06:50:18 AM UTC

--- ensyu2.8/ensyu2.8.f90
+++ ensyu2.8/ensyu2.8.f90
@@ -8,19 +8,19 @@ program tri_surface
     read(fi, *) b
     close(fi)
     c = b - a
-    a_n = sqrt(norm2(a))
-    b_n = sqrt(norm2(b))
-    c_n = sqrt(norm2(c))
+    a_n = norm2(a)
+    b_n = norm2(b)
+    c_n = norm2(c)
     s = (a_n + b_n + c_n) * 0.5
     do i = 1, 3
         axb(i) = a(mod(i, 3) + 1) * b(mod(i + 1, 3) + 1) - a(mod(i + 1, 3) + 1) * b(mod(i, 3) + 1)
     end do
-    area1 = sqrt(norm2(axb)) * 0.5
+    area1 = norm2(axb) * 0.5
     area2 = sqrt(s * (s - a_n) * (s - b_n) * (s - c_n))
     write(*, *) 'a_n =', a_n
     write(*, *) 'b_n =', b_n
     write(*, *) 'c_n =', c_n
     write(*, *) 's =', s
     write(*, *) 'area1(cross_product) =', area1
-    write(*, *) ' area2(heron) =', area2 
+    write(*, *) 'area2(heron) =', area2 
 end program
