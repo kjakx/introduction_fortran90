77e54526878ed9ea1920834578f84964f7151259
kjakx <fxhfr163@ybb.ne.jp>
2019-12-30 14:33:59 +0000

    saved@Mon 30 Dec 2019 11:33:58 PM JST

--- list2.8/list2.8.f90
+++ list2.8/list2.8.f90
@@ -1,4 +1,17 @@
 program random_umat
     implicit none
     double precision, allocatable :: a(:, :)
-    
+    integer n, i, j 
+    write(*, *) "input n (1<=n<=100):"
+    read(*, *) n 
+    if (n < 1 .or. 100 < n) stop "stop. n is invalid"
+    allocate(a(n, n))
+    call random_seed
+    do j = 1, n 
+        call random_number(a(1:j, j))
+        a(j+1:n, j) = 0.0d0
+    end do 
+    do i = 1, n 
+        write(*, '(100e12.4)') a(i, :)
+    end do
+end program random_umat   
