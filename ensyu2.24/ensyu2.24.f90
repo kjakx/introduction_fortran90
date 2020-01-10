d31bbb0bb7b043f278a3370257a8f3ff03c7c5f5
kjakx <fxhfr163@ybb.ne.jp>
2020-01-10 00:44:21 +0000

    saved@Fri 10 Jan 2020 09:44:21 AM JST

--- ensyu2.24/ensyu2.24.f90
+++ ensyu2.24/ensyu2.24.f90
@@ -1,16 +1,21 @@
 program outer_product
     implicit none
-    integer i, j
+    integer i
     double precision a(3), b(3), c(2, 3), d(2, 2), outer(3)
-    call random_seed 
-    call random_number(a)
-    call random_number(b)
+    a(:) = (/1, 2, 3/)
+    b(:) = (/4, 5, 6/)
     c(1, :) = a(:)
     c(2, :) = b(:)
+    write(*, *) "a :", a
+    write(*, *) "b :", b
+    write(*, *) "c :"
+    do i = 1, 2
+        write(*, *) c(i, :)
+    end do
     do i = 1, 3
         d(:, :) = c(:, 2:3)
         outer(i) = d(1, 1) * d(2, 2) - d(1, 2) * d(2, 1)
-        c = cshift(c, 1, 1)
+        c = cshift(c, (/1, 1/), 2)
     end do 
     write(*, *) "outer_product =", (outer(i), i = 1, 3)
 end program outer_product
