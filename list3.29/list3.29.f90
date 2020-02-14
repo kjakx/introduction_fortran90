2d104d7ee2e9a0903c69af94f64f68f9f824801c
kjakx <fxhfr163@ybb.ne.jp>
2020-02-14 02:50:38 +0000

    saved@Fri 14 Feb 2020 11:50:37 AM JST

---
+++ list3.29/list3.29.f90
@@ -0,0 +1,29 @@
+module globals
+    double precision, allocatable, save :: a(:, :)
+    integer, save :: n, m
+end module globals
+
+module mat_subprogs2
+    use globals
+    implicit none
+contains
+    subroutine allocate_rmat2
+        write(*, *) 'input n, m :'
+        read(*, *) n, m
+        allocate(a(n, m))
+        call random_number(a)
+    end subroutine allocate_rmat2
+
+    subroutine print_mat3
+        integer i
+        do i = 1, n
+            write(*, '(100e12.4)') a(i, 1:m)
+        end do
+    end subroutine print_mat3
+end module mat_subprogs2
+
+program random_mod2
+    use mat_subprogs2
+    call allocate_rmat2
+    call print_mat3
+end program random_mod2
